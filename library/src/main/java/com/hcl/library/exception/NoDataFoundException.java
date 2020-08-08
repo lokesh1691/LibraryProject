d0fd766d53c670284952c33ca32a212cc00d8dcc
Mohan <mohanb.vayyala@gmail.com>
2020-08-08 14:15:51 +0530

    added exception classes

---
+++ library/src/main/java/com/hcl/library/exception/NoDataFoundException.java
@@ -0,0 +1,15 @@
+package com.hcl.library.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class NoDataFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 6035891000374756096L;
+
+	public NoDataFoundException() {
+        super("No data found");
+    }
+
+}
